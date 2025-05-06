beafe1dcaa2ccdaff7b7237501b8c46c3636754b
UnknownBart999 <bwolek7848@gmail.com>
2025-05-06 23:44:02 +0200

    Added function to check if a user can be created. Modified create_user to accept roleId.

--- user_managment.py
+++ user_managment.py
@@ -1,17 +1,74 @@
+# Change log V1 -> V2: add imports for engine, bcrypt. Change import from models to database_I
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import insert, select, update, delete, engine
 from sqlalchemy.orm import sessionmaker
 from database_I import *
 import bcrypt
 
+
+# Check if student/teacher/staff account can be created.
+# Change log V1: created function to test combinations of uuid, roleId, and role for validity.
+def can_create_user(engine: Engine, uuid: int, roleId: int, role: Roles) -> (bool, str):
+    """Checks if student/teacher/staff account can be created.
+    
+    Params:
+    engine: db engine
+    uuid: id to check if in db
+    role: role of id
+    
+    Returns:
+    (bool, str): (can be created, error message)
+    """
+    
+    with Session(engine) as session:
+        
+        # Check if user id exists in User table
+        check_users = select(User).where(User.userId == uuid)
+        
+        if session.execute(check_users).first() is not None: 
+            return (False, "User Id exists in User table.") 
+        
+        
+        # Check that an account for role id and role does not already exist.
+        check_role_acc = select(User).where(User.roleId == roleId).where(User.role == role)
+        
+        if session.execute(check_role_acc).first() is not None:
+            return (False, "Account for Role and Role ID exists.")
+           
+            
+        # Check if role id exists in role table.
+        check_role_id = None
+        
+        if role == Roles.STUDENT:
+            check_role_id = select(Student).where(Student.studentId == roleId)
+        elif role == Roles.TEACHER:
+            check_role_id = select(Teacher).where(Teacher.teacherId == roleId)
+        elif role == Roles.STAFF:
+            # Howd i forget to imp a Staff table???
+            # check_role_id = select(Staff).where(Staff.staffId == roleId)
+            return (False, "Staff not implemented.")
+        else:
+            return (False, "Invalid role.")
+        
+        if session.execute(check_role_id).first() is None:
+            return (False, "ID does not exist in corresponding Role table.")
+        
+        
+    # If this statement is reached, the account can be created.
+    return (True, "None")
+        
+
 # Create User
-# Change log: create function for creating a user, while auto encrypting the password.
-def create_user(engine, uuid: int, username: str, password: str, role: Roles) -> bool:
+# Change log V1: create function for creating a user, while auto encrypting the password.
+# Change log V1 -> V2: update to check if params are adequate, and add roleId param.
+def create_user(engine, uuid: int, roleId: int, username: str, password: str, role: Roles) -> bool:
     """Create a new user. Automatically encrypts password.
     
     Params:
     engine: sqlalchemy engine to connect to db.
-    uuid: id number for student/teacher/staff member.
+    uuid: id number for user table
+    roleId: id number for student/teacher/staff member.
     username: string
     password: string
     role: STUDENT/TEACHER/STAFF
@@ -20,25 +77,31 @@ def create_user(engine, uuid: int, username: str, password: str, role: Roles) ->
     True if the user was created sucessfully. False otherwise.
     """
     
+    # Test if passed params can create a user.
+    test_params = can_create_user(engine, uuid, roleId, role)
+    if not test_params[0]:
+        print(f"Error: {test_params[1]}")
+        return False
+    
+    # Encrypt password
     hashed = password.encode()
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(hashed, salt)
+    
+    # Create User
+    with Session(engine) as session:
 
-    output = True
+        user = User(userId=uuid, roleId=roleId, username=username, password=hashed, role=role)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        # If creating user causes an error, return false
+        try:
+            session.add(user)
+            session.commit()
+        except Exception as e:
+            return False
 
-    user = User(id=uuid, username=username, password=hashed, role=role)
-
-    try:
-        session.add(user)
-        session.commit()
-    except Exception as e:
-        output = False
-
-    session.close()
-    return output
+    # If statement is reached, user was created.
+    return True
 
 
 # Insert data
